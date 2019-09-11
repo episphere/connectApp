4ae8d807966205bcdeef3412d4cdcdf61457e0ac
bhaumik55231 <bhaumik55231@gmail.com>
2019-09-11 16:12:28 -0400

    update to password authentication

--- app.js
+++ app.js
@@ -434,11 +434,7 @@ const signInConfig = () => {
         signInOptions: [
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
             firebase.auth.GithubAuthProvider.PROVIDER_ID,
-            {
-                provider:firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
-                forceSameDevice: true,
-            },
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
             firebase.auth.PhoneAuthProvider.PROVIDER_ID
         ]
     }
