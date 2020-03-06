9bdfd7330d9a6013ea5aadf5ffddfa4ba95b79a6
bhaumik55231 <bhaumik55231@gmail.com>
2020-03-06 10:58:36 -0500

    minor update

--- js/components/form.js
+++ js/components/form.js
@@ -10,34 +10,34 @@ export const renderUserProfile = async () => {
     document.body.appendChild(js);
     mainContent.innerHTML = `
         </br></br>
-        <h4>Contact Information</h4>
+        <h2>Contact Information</h2>
         The National Cancer Institute and ${siteId ? sites()[siteId] : ''} thank you for your participation in this important study. 
             Before we begin, we will need some basic contact information. We will only use this information to contact you about study-related activities and updates. 
             We will not share your contact information with other people or groups.
         
         <form id="userProfileForm" method="POST">
             <div class="form-group row">
-                <label class="col-sm-3 col-form-label"><strong>First name</strong> <span class="required">*</span></label>
+                <label class="col-sm-3 col-form-label">First name <span class="required">*</span></label>
                 <input type="text" class="form-control col-sm-4" required id="UPFirstName" placeholder="Enter first name">
                 
             </div>
             <div class="form-group row">
-                <label class="col-sm-3 col-form-label"><strong>Middle initial</strong></label>
+                <label class="col-sm-3 col-form-label">Middle initial</label>
                     <input type="text" class="form-control col-sm-4" id="UPMiddleInitial" placeholder="Enter middle initial">
             </div>
             <div class="form-group row">
-                <label class="col-sm-3 col-form-label"><strong>Last name</strong> <span class="required">*</span></label>
+                <label class="col-sm-3 col-form-label">Last name <span class="required">*</span></label>
                 <input type="text" class="form-control col-sm-4" required id="UPLastName" placeholder="Enter last name">
                 
             </div>
             <div class="form-group row">
-                <label class="col-sm-3 col-form-label"><strong>Suffix</strong></label>
+                <label class="col-sm-3 col-form-label">Suffix</label>
                 <input type="text" class="form-control col-sm-4" id="UPSuffix" placeholder="Enter suffix">
             </div>
             
-            <strong>Date of Birth</strong>
+            <h4>Date of Birth</h4>
             <div class="form-group row">
-                <label class="col-sm-3 col-form-label"><strong>Month </strong><span class="required">*</span></label>
+                <label class="col-sm-3 col-form-label">Month <span class="required">*</span></label>
                 <select class="form-control col-sm-4" required id="UPMonth">
                     <option value="">-- Select Month --</option>
                     <option value="01">JAN</option>
@@ -56,29 +56,29 @@ export const renderUserProfile = async () => {
             </div>
 
             <div class="form-group row">
-                <label class="col-sm-3 col-form-label"><strong>Day </strong><span class="required">*</span></label>
+                <label class="col-sm-3 col-form-label">Day <span class="required">*</span></label>
                 <select class="form-control col-sm-4" required id="UPDay">
                     
                 </select>
             </div>
             <div class="form-group row">
-                <label class="col-sm-3 col-form-label"><strong>Year </strong><span class="required">*</span></label>
+                <label class="col-sm-3 col-form-label">Year <span class="required">*</span></label>
                 <input type="text" required class="form-control col-sm-4" id="UPYear" pattern="19[0-9]{2}" title="Birth year, must be in 1900s" Placeholder="Enter birth year">
             </div>
-
+            
+            Later in this survey, we will ask about some health conditions that are related to a person’s biological sex (male or female). We want to make sure that you are asked the right questions.  Later in the questionnaires, there will be an opportunity to tell us more about your gender and sexual orientation.
             <div class="form-group row">
-                Later in this survey, we will ask about some health conditions that are related to a person’s biological sex (male or female). We want to make sure that you are asked the right questions.  Later in the questionnaires, there will be an opportunity to tell us more about your gender and sexual orientation
-                <label class="col-form-label"><strong>At birth, how was your physical sex assigned? </strong><span class="required">*</span></label>
-                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="col-sm-5 col-form-label">At birth, how was your physical sex assigned? <span class="required">*</span></label>
+                <div class="btn-group btn-group-toggle col-sm-4" data-toggle="buttons">
                     <label class="btn btn-light"><input type="radio" required name="UPRadio" id="UPMale" value="0">Male</label>
                     <label class="btn btn-light"><input type="radio" required name="UPRadio" id="UPFemale" value="1">Female</label>
                     <label class="btn btn-light"><input type="radio" required name="UPRadio" id="UPOther" value="2">Intersex or other</label>
                 </div>
             </div>
 
-            <strong>How can we reach you?</strong>
+            <h4>How can we reach you?</h4>
             <div class="form-group row">
-                <label class="col-sm-3 col-form-label"><strong>Email address</strong> <span class="required">*</span></label>
+                <label class="col-sm-3 col-form-label">Email address <span class="required">*</span></label>
                 <input type="email" class="form-control col-sm-4" required id="UPEmail" Placeholder="Enter email"></br>
             </div>
             <div id="multipleEmail">
@@ -94,7 +94,7 @@ export const renderUserProfile = async () => {
             </div>
 
             <div class="form-group">
-                <label><strong>What is your preferred method of contact?</strong></label>
+                <label>What is your preferred method of contact?</label>
                 <div class="btn-group btn-group-toggle" data-toggle="buttons" id="methodOfContactFields">
                     <label class="btn btn-light"><input type="radio" name="methodOfContact" id="UPMethodOfContactEmail1" >Email 1</label>
                     <label class="btn btn-light"><input type="radio" name="methodOfContact" id="UPMethodOfContactEmail2" >Email 2</label>
@@ -128,19 +128,19 @@ export const renderAlternateContact = (id, required) => {
     return `
         <div class="form-group">
             <label>
-                <strong>First name</strong> ${required ? '<span class="required">*</span>': ''}
+                First name ${required ? '<span class="required">*</span>': ''}
                 <input type="text" class="form-control" ${required ? 'required' : ''} id="UPFirstName${id}" placeholder="Enter first name">
             </label><br>
             <label>
-                <strong>Middle initial</strong>
+                Middle initial
                 <input type="text" class="form-control" id="UPMiddleInitial${id}" placeholder="Enter middle initial">
             </label><br>
             <label>
-                <strong>Last name</strong> ${required ? '<span class="required">*</span>': ''}
+                Last name ${required ? '<span class="required">*</span>': ''}
                 <input type="text" class="form-control" ${required ? 'required' : ''} id="UPLastName${id}" placeholder="Enter last name">
             </label><br>
             <label>
-                <strong>Phone number</strong> ${required ? '<span class="required">*</span>': ''}
+                Phone number ${required ? '<span class="required">*</span>': ''}
                 <input type="text" class="form-control" id="UPPhoneNumber${id}" ${required ? 'required' : ''} pattern="[1-9]{1}[0-9]{9}" size="10" maxlength="10" Placeholder="Enter phone number">
             </label><br>
         </div>
@@ -150,28 +150,28 @@ export const renderAlternateContact = (id, required) => {
 
 export const renderMailingAddress = (type, id, required, showCountry) => {
     return `
-        <strong>${type}Mailing Address</strong>
+        <h4>${type}Mailing Address</h4>
         <div class="form-group row">
             <label class="col-sm-3 col-form-label">
-                <strong>Line 1 (street, PO box, rural route)</strong> ${required ? '<span class="required">*</span>': ''}
+                Line 1 (street, PO box, rural route) ${required ? '<span class="required">*</span>': ''}
             </label>
             <input type=text id="UPAddress${id}Line1" autocomplete="off" class="form-control col-sm-4" ${required ? 'required' : ''} placeholder="Enter street, PO box, rural route">
         </div>
         <div class="form-group row">
             <label class="col-sm-3 col-form-label">
-                <strong>Line 2 (apartment, suite, unit, building)</strong>
+                Line 2 (apartment, suite, unit, building)
             </label>
             <input type=text id="UPAddress${id}Line2" autocomplete="off" class="form-control col-sm-4" placeholder="apartment, suite, unit, building">
         </div>
         <div class="form-group row">
             <label class="col-sm-3 col-form-label">
-                <strong>City</strong> ${required ? '<span class="required">*</span>': ''}
+                City ${required ? '<span class="required">*</span>': ''}
             </label>
             <input type=text id="UPAddress${id}City" class="form-control col-sm-4" ${required ? 'required' : ''} placeholder="Enter City">
         </div>
         <div class="form-group row">
             <label class="col-sm-3 col-form-label">
-                <strong>State</strong> ${required ? '<span class="required">*</span>': ''}
+                State ${required ? '<span class="required">*</span>': ''}
             </label>
             <select class="form-control col-sm-4" ${required ? 'required' : ''} id="UPAddress${id}State">
                 <option value="">-- Select State --</option>
@@ -180,7 +180,7 @@ export const renderMailingAddress = (type, id, required, showCountry) => {
         </div>
         <div class="form-group row">
             <label class="col-sm-3 col-form-label">
-                <strong>Zip</strong> ${required ? '<span class="required">*</span>': ''}
+                Zip ${required ? '<span class="required">*</span>': ''}
             </label>
             <input type=text id="UPAddress${id}Zip" pattern="[0-9]{5}" class="form-control col-sm-4" size="5" maxlength="5" ${required ? 'required' : ''} placeholder="Enter zip">
             
@@ -188,7 +188,7 @@ export const renderMailingAddress = (type, id, required, showCountry) => {
         ${showCountry ? `<br>
         <div class="form-group row">
             <label class="col-sm-3 col-form-label">
-                <strong>Country</strong> ${required ? '<span class="required">*</span>': ''}
+                Country ${required ? '<span class="required">*</span>': ''}
             </label>
             <select class="form-control col-sm-4" ${required ? 'required' : ''} id="UPAddress${id}Country">
                 <option value="">-- Select Country --</option>
@@ -204,13 +204,13 @@ export const renderPhoneNumber = (number) => {
     return `
     <div class="form-group row">
         <label class="col-sm-3 col-form-label">
-            <strong>Phone number ${number}</strong>
+            Phone number ${number}
         </label>
         <input type="text" class="form-control col-sm-4" id="UPPhoneNumber${number}" pattern="[1-9]{1}[0-9]{9}" size="10" maxlength="10" Placeholder="Enter phone number ${number}">
     </div>
     <div class="form-group row">
         <label class="col-sm-3 col-form-label">
-            <strong>Phone number ${number} type</strong>
+            Phone number ${number} type
         </label>
         <div class="btn-group btn-group-toggle col-sm-4" data-toggle="buttons">
             <label class="btn btn-light"><input type="radio" name="phoneNumberType${number}" id="UPPhoneType${number}1" value="1">Mobile</label>
