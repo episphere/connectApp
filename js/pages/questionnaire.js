fc74c61d7020cd17b5e13cfd486a997b1c57fcb9
bhaumik55231 <bhaumik55231@gmail.com>
2020-05-27 15:35:18 -0400

    placeholder module1 through module4

--- js/pages/questionnaire.js
+++ js/pages/questionnaire.js
@@ -3,7 +3,34 @@ import { transform } from 'https://episphere.github.io/quest/replace2.js';
 
 export const questionnaire = () => {
     transform.render({
-        url: 'https://jonasalmeida.github.io/privatequest/mod1_abridged.txt',
+        url: 'https://episphere.github.io/quest/questionnaires/testQuest.txt',
+        activate: true,
+        store: storeResponse,
+        retrieve: getMyData
+    }, 'root');
+}
+
+export const questionnaire2 = () => {
+    transform.render({
+        url: 'https://episphere.github.io/quest/questionnaires/testQuest.txt',
+        activate: true,
+        store: storeResponse,
+        retrieve: getMyData
+    }, 'root');
+}
+
+export const questionnaire3 = () => {
+    transform.render({
+        url: 'https://episphere.github.io/quest/questionnaires/testQuest.txt',
+        activate: true,
+        store: storeResponse,
+        retrieve: getMyData
+    }, 'root');
+}
+
+export const questionnaire4 = () => {
+    transform.render({
+        url: 'https://episphere.github.io/quest/questionnaires/testQuest.txt',
         activate: true,
         store: storeResponse,
         retrieve: getMyData
