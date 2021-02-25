6dc681c9b40c1bf8cf781ddb5009acbee00a3cdd
hzhao392 <57492029+hzhao392@users.noreply.github.com>
2021-02-24 22:39:07 -0500

    enabling questions based on logic

--- js/pages/myToDoList.js
+++ js/pages/myToDoList.js
@@ -4,6 +4,7 @@ import { renderUserProfile } from "../components/form.js";
 import { consentTemplate, initializeCanvas, addEventConsentSubmit } from "./consent.js";
 import { addEventsConsentSign, addEventHeardAboutStudy, addEventRequestPINForm, addEventHealthCareProviderSubmit, addEventPinAutoUpperCase } from "../event.js";
 import { heardAboutStudy, requestPINTemplate, healthCareProvider } from "./healthCareProvider.js";
+import {humanReadableMDYwithTime} from "../util.js";
 
 export const myToDoList = (data) => {
     const mainContent = document.getElementById('root');
@@ -30,15 +31,16 @@ export const myToDoList = (data) => {
                 <div style="border: 1px solid #dee2e6; padding: 20px; border-radius:0px 10px 10px 10px;" id="surveyMainBody">
                 `;
                 const modules = questionnaireModules;
-                if (data.Module1 && data.Module1.COMPLETED) { modules["Enter SSN"].enabled = true};
-                if (data.ModuleSsn && data.ModuleSsn.COMPLETED) { modules["Medications, Reproductive Health, Exercise, and Sleep"].enabled = true};
-                if (data.Module2 && data.Module2.COMPLETED) { modules["Smoking, Alcohol, and Sun Exposure"].enabled = true};
-                if (data.Module3 && data.Module3.COMPLETED) { modules["Where You Live and Work"].enabled = true};
-                for(let key in modules){
-                    template += `<li class="list-item">
-                                    <button class="btn list-item-active btn-agreement questionnaire-module ${modules[key].enabled ? '' : 'btn-disbaled'}" title="${key}" module_id="${modules[key].moduleId}" data-module-url="${modules[key].url ? modules[key].url : ''}">${key}</button>
-                                </li>`;
-                }
+                // if (data.Module1 && data.Module1.COMPLETED) { modules["Enter SSN"].enabled = true};
+                // if (data.ModuleSsn && data.ModuleSsn.COMPLETED) { modules["Medications, Reproductive Health, Exercise, and Sleep"].enabled = true};
+                // if (data.Module2 && data.Module2.COMPLETED) { modules["Smoking, Alcohol, and Sun Exposure"].enabled = true};
+                // if (data.Module3 && data.Module3.COMPLETED) { modules["Where You Live and Work"].enabled = true};
+                // for(let key in modules){
+                //     template += `<li class="list-item">
+                //                     <button class="btn list-item-active btn-agreement questionnaire-module ${modules[key].enabled ? '' : 'btn-disbaled'}" title="${key}" module_id="${modules[key].moduleId}" data-module-url="${modules[key].url ? modules[key].url : ''}">${key}</button>
+                //                 </li>`;
+                // }
+                template += renderMainBody(data, 'todo')
                 template += `</ul>`
 
                 // template += `
@@ -142,33 +144,43 @@ const renderMainBody = (data, tab) => {
             <ul class="questionnaire-module-list">`;
     const modules = questionnaireModules;
     modules['Background and Overall Health'].description = 'Answer basic questions about your health and status.';
-    modules['Enter SSN'].description = 'Enter your SSN';
+    
     modules['Medications, Reproductive Health, Exercise, and Sleep'].description = 'Answer questions about your medications, health, exercise, and sleep.';
     modules['Smoking, Alcohol, and Sun Exposure'].description = 'Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.Answer questions about your smoking and alcohol intake.';
     modules["Where You Live and Work"].description  = 'Answer questions about your living and work environment'
+    modules['Enter SSN'].description = 'Enter your SSN';
     
     if (data.Module1 && data.Module1.COMPLETED) { 
-        modules["Enter SSN"].enabled = true;
+        modules["Smoking, Alcohol, and Sun Exposure"].enabled = true;
+        modules["Where You Live and Work"].enabled = true;
+        modules['Medications, Reproductive Health, Exercise, and Sleep'].enabled = true;
         modules['Background and Overall Health'].completed = true;
     };
-    if (data.ModuleSsn && data.ModuleSsn.COMPLETED) { 
-        modules["Medications, Reproductive Health, Exercise, and Sleep"].enabled = true;
-        modules['Enter SSN'].completed = true;
-    };
     if (data.Module2 && data.Module2.COMPLETED) { 
-        modules["Smoking, Alcohol, and Sun Exposure"].enabled = true
         modules['Medications, Reproductive Health, Exercise, and Sleep'].completed = true;
     };
     if (data.Module3 && data.Module3.COMPLETED) { 
-        modules["Where You Live and Work"].enabled = true
         modules['Smoking, Alcohol, and Sun Exposure'].completed = true;
     };
     if (data.Module4 && data.Module4.COMPLETED) { 
         modules["Where You Live and Work"].completed  = true
     };
+    if (data.Module1 && data.Module1.COMPLETED 
+        && data.Module2 && data.Module2.COMPLETED
+        && data.Module3 && data.Module3.COMPLETED
+        && data.Module4 && data.Module4.COMPLETED) { 
+        modules['Enter SSN'].enabled = true;
+    };
+    if (data.ModuleSsn && data.ModuleSsn.COMPLETED) { 
+        modules['Enter SSN'].completed = true;
+    };
+
+
     if(tab === 'todo'){
+        let hasModlesRemaining = false;
         for(let key in modules){
             if(!modules[key].completed){
+            hasModlesRemaining = true
             template += `<li style="width:100%; margin:auto; margin-bottom:20px; border:1px solid lightgrey; border-radius:5px;">
                             <div class="row">
                                 <div class="col-1">
@@ -187,7 +199,7 @@ const renderMainBody = (data, tab) => {
                                 </div>
                                
                                 <div class="col-3">
-                                <button class="btn survey-list-active btn-agreement questionnaire-module ${modules[key].enabled ? 'list-item-active' : 'btn-disbaled survey-list-inactive'}" title="${key}" data-module-url="${modules[key].url ? modules[key].url : ''}" style=""><b>Start</b></button>
+                                <button class="btn survey-list-active btn-agreement questionnaire-module ${modules[key].enabled ? 'list-item-active' : 'btn-disbaled survey-list-inactive'}" title="${key}" module_id="${modules[key].moduleId}" data-module-url="${modules[key].url ? modules[key].url : ''}" style=""><b>Start</b></button>
                                 </div>
                             </div>
                             <div class="row">
@@ -199,11 +211,18 @@ const renderMainBody = (data, tab) => {
                         </li>`;*/
             }
         }
+        if (!hasModlesRemaining){
+            template += /*html*/`
+                            <div class="row">
+                                Thank you for completing your surveys
+                            </div>
+                            `
+        }
     }
     else{
         for(let key in modules){
             if(modules[key].completed){
-                template += `<li style="width:100%; margin:auto; margin-bottom:20px; border:1px solid lightgrey; border-radius:5px;">
+                template += /*html*/ `<li style="width:100%; margin:auto; margin-bottom:20px; border:1px solid lightgrey; border-radius:5px;">
                     <div class="row">
                         <div class="col-1">
                         <i class="fas fa-clipboard-list" title="Survey Icon" style="margin-left:10px; font-size:50px;color:#c2af7f;"></i>
@@ -221,6 +240,7 @@ const renderMainBody = (data, tab) => {
                         </div>
                     
                         <div class="col-3">
+                         Completed Time: ${humanReadableMDYwithTime(data[modules[key].moduleId].COMPLETED_TS)}
                         <!--
                         <button class="btn list-item-active btn-agreement questionnaire-module ${modules[key].enabled ? '' : 'btn-disbaled'}" title="${key}" data-module-url="${modules[key].url ? modules[key].url : ''}" style="margin-top:0px;border-radius:30px; height:60px;background-color:#5c2d93 !important;color:white; width:100%"><b>Review</b></button>
                         -->
