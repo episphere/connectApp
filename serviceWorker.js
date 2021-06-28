4cb4d6f02336aee196245b01c5d4832de517e520
bhaumik55231 <bhaumik55231@gmail.com>
2021-06-28 13:53:03 -0400

    adding gtags for dev, stage and prod

--- serviceWorker.js
+++ serviceWorker.js
@@ -46,4 +46,4 @@ registerRoute(
     'POST'
 );
 
-workbox.precaching.precacheAndRoute([{url: 'index.html', revision: 'beta-v1.0.5'}]);
+workbox.precaching.precacheAndRoute([{url: 'index.html', revision: 'beta-v1.0.6'}]);
