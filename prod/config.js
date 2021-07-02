ab64523af2df16f9b2989953ca0dfb34d0492203
bhaumik55231 <bhaumik55231@gmail.com>
2021-07-02 12:48:16 -0400

    updating prod configs

--- prod/config.js
+++ prod/config.js
@@ -1,5 +1,5 @@
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyDjFzceC2w6e9SPrSgx8_jDWnOpHdJbTGs",
-    authDomain: "nih-nci-dceg-connect-stg-5519.firebaseapp.com"
+    apiKey: "AIzaSyBczpNilKrzA97gMwYvZnMVSQQIxxltarI",
+    authDomain: "nih-nci-dceg-connect-prod-6d04.firebaseapp.com"
 };
