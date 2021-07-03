d959e89683d07a5746fae889e84e620026c653cd
bhaumik55231 <bhaumik55231@gmail.com>
2021-07-03 07:25:48 -0400

    Updating SOCcer URL

--- stage/config.js
+++ stage/config.js
@@ -3,3 +3,5 @@ export const firebaseConfig = {
     apiKey: "AIzaSyDjFzceC2w6e9SPrSgx8_jDWnOpHdJbTGs",
     authDomain: "nih-nci-dceg-connect-stg-5519.firebaseapp.com"
 };
+
+export const SOCcer = 'https://soccer-myconnect-stage.cancer.gov/soccer/code?title='
