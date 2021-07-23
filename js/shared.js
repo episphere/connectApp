03e3512df6133bf71c905360240b6ec1ce121307
bhaumik55231 <bhaumik55231@gmail.com>
2021-07-23 09:25:24 -0400

    update ssn module url

--- js/shared.js
+++ js/shared.js
@@ -757,10 +757,10 @@ export const getConceptVariableName = async (conceptId) => {
 //for submodules use https://raw.githubusercontent.com/jonasalmeida/privatequest/master/submodules/module1_config.txt?token=ANWUEPNBOGO6ATOXSNG5DWDAWZ6XC
 export const questionnaireModules = {
     'Background and Overall Health': {url: 'https://raw.githubusercontent.com/episphere/questionnaire/main/module1.txt', moduleId:"Module1", enabled:true},
-    'Medications, Reproductive Health, Exercise, and Sleep': {url: 'https://raw.githubusercontent.com/episphere/questionnaire/main/module2.txt', moduleId:"Module2", enabled:false},
+    'Medications, Reproductive Health, Exercise, and Sleep': {url: 'https://raw.githubusercontent.com/episphere/questionnaire/main/module2.txt', moduleId:"Module2", enabled:true},
     'Smoking, Alcohol, and Sun Exposure': {url: 'https://hzhao392.github.io/privatequest/test_module3.txt', moduleId:"Module3", enabled:false},
     'Where You Live and Work': {url: 'https://hzhao392.github.io/privatequest/test_module4.txt', moduleId:"Module4", enabled:false},
-    'Enter SSN': {url: 'https://jonasalmeida.github.io/privatequest/ssnModule.txt', moduleId:"ModuleSsn", enabled:false}
+    'Enter SSN': {url: 'https://raw.githubusercontent.com/episphere/questionnaire/main/ssnModule.txt', moduleId:"ModuleSsn", enabled:false}
 }
 
 export const isBrowserCompatible = () => {
