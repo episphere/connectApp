ae79b9cafea9337cba93decbdad48141d92fae80
Shen <shenbn@nci-02134517-ml.fios-router.home>
2021-07-26 12:12:32 -0400

    adding the duplicate check

--- js/pages/myToDoList.js
+++ js/pages/myToDoList.js
@@ -152,6 +152,20 @@ export const myToDoList = (data, fromUserProfile) => {
                     hideAnimation();
                     return;
                 }
+                else if(data['821247024'] && data['821247024'] == 922622075) {
+                    template += `
+                    <div class="alert alert-warning" id="verificationMessage" style="margin-top:10px;">
+                        Our records show that there is another account linked to your sign in information. Please sign out of this account and sign in again using a different email address or phone number. If you cannot remember the information you used to create your account, please contact the Connect Support Center by emailing <a href = "mailto:ConnectSupport@norc.org">ConnectSupport@norc.org</a> or calling 1-877-505-0253.
+                    </div>
+                    </div>
+                    <div class="col-lg-2">
+                    </div>
+                    </div>
+                    `
+                    mainContent.innerHTML = template;
+                    hideAnimation();
+                    return;
+                }
                 else if(data['821247024'] && data['821247024'] == 160161595) {
                     let sites = sites();
                     template += `
