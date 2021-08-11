3b48ee28b9d57f7d25fde3479bf3935dd270cde7
Shen <shenbn@nci-02134517-ml.fios-router.home>
2021-08-11 11:31:36 -0400

    centering footer

--- js/pages/footer.js
+++ js/pages/footer.js
@@ -1,13 +1,13 @@
 export const footerTemplate = () => {
     return `
-        <div class="row footer-content">
-            <div class="row w-100">
+        <div class="row footer-content" style="margin:0">
+            <div class="row w-100" style="margin:0">
                 <div class="col content-wrapper">
                     <div class="dceg-footer align-left text-center text-sm-left">Division of Cancer Epidemiology and Genetics</div>
                     <div class="nci-dceg-footer align-left text-center text-sm-left">at the National Cancer Institute</div>
                 </div>
             </div>
-            <div class="row w-100 pt-0 align-left">
+            <div class="row w-100 pt-0 align-left" style="margin:0">
                 <div class="col-lg-6 pt-3 content-wrapper text-center text-sm-left">
                     <div class="footer-heading">HAVE QUESTIONS?</div>
                     <div class="footer-sub-heading pt-3"><a class="footer-links" target="__blank" href="https://norcfedramp.servicenowservices.com/recruit">Contact the Connect Support Center</a></div>
@@ -22,7 +22,7 @@ export const footerTemplate = () => {
                     <div class="footer-sub-heading pt-3"><a class="footer-links" target="__blank" href="https://dceg.cancer.gov/about/sitemap">Site Map</a></div>
                 </div>
             </div>
-            <div class="row w-100 pt-5 align-center">
+            <div class="row w-100 pt-5 align-center" style="margin:0">
                 <ul class="menu w-100">
                     <li class="menu-item p-0 footer-menu-item"><a target="__blank" class="footer-links gov-dept-links p-0" href="http://www.hhs.gov/">U.S. Department of Health and Human Services</a></li>
                     <li class="menu-item p-0 footer-menu-item"><a target="__blank" class="footer-links gov-dept-links p-0" href="http://www.nih.gov/">National Institutes of Health</a></li> 
