5bba0a4a6be3863d972f500bfb9433d676fd57b8
bhaumik55231 <bhaumik55231@gmail.com>
2021-09-27 15:27:49 -0400

    disabling Google and Email sign in mechanisms

--- app.js
+++ app.js
@@ -318,11 +318,6 @@ const signInConfig = () => {
     return {
         signInSuccessUrl: '#dashboard',
         signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            {
-                provider:firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
-            },
             firebase.auth.PhoneAuthProvider.PROVIDER_ID
         ],
         credentialHelper: 'none'
