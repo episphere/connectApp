c902af90537b081bb4363c62d91ebd423796e279
Shen <shenbn@nci-02134517-ml.fios-router.home>
2021-12-16 09:06:27 -0500

    removing yellow bar when no text

--- js/pages/myToDoList.js
+++ js/pages/myToDoList.js
@@ -78,7 +78,7 @@ export const myToDoList = (data, fromUserProfile) => {
                         finalMessage += "You have been withdrawn from Connect per your request.<br>"
                     }
                 }
-                if(finalMessage !== ""){
+                if(finalMessage.trim() !== ""){
                     template += `
                     <div class="alert alert-warning" id="verificationMessage" style="margin-top:10px;">
                         ${finalMessage}
@@ -232,7 +232,7 @@ export const myToDoList = (data, fromUserProfile) => {
                     
                 }
                 
-                if(topMessage !== ""){
+                if(topMessage.trim() !== ""){
                     template += `
                     <div class="alert alert-warning" id="verificationMessage" style="margin-top:10px;">
                         ${topMessage}
