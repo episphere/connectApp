7a7bfc899636382f9a8c92afa263bdbde41fe413
Shen <shenbn@nci-02134517-ml.fios-router.home>
2022-01-05 10:35:32 -0500

    fixing typo

--- js/shared.js
+++ js/shared.js
@@ -837,8 +837,8 @@ export const questionnaireModules = () => {
     }
     else{
         return {
-            'Background and Overall Health': {url: 'https://raw.githubusercontent.com/episphere/questionnaire/main/module1Dev.txt', moduleId:"Module1", enabled:true},
-            'Medications, Reproductive Health, Exercise, and Sleep': {url: 'https://raw.githubusercontent.com/jonasalmeida/privatequest/master/module2_concept_id.txt?token=AGOJYPCSF2MSVW75IPMQLHDBT5RDA', moduleId:"Module2", enabled:false},
+            'Background and Overall Health': {url: 'https://raw.githubusercontent.com/episphere/questionnaire/main/module1Stage.txt', moduleId:"Module1", enabled:true},
+            'Medications, Reproductive Health, Exercise, and Sleep': {url: 'https://raw.githubusercontent.com/episphere/questionnaire/main/module2Dev.txt', moduleId:"Module2", enabled:false},
             'Smoking, Alcohol, and Sun Exposure': {url: 'https://hzhao392.github.io/privatequest/test_module3.txt', moduleId:"Module3", enabled:false},
             'Where You Live and Work': {url: 'https://hzhao392.github.io/privatequest/test_module4.txt', moduleId:"Module4", enabled:false},
             'Enter SSN': {url: 'https://raw.githubusercontent.com/episphere/questionnaire/main/ssnModule.txt', moduleId:"ModuleSsn", enabled:false}
