d2e9072e94a207e3e4c9b8eedbb073d141d346b4
Brian S <27040828+naiyume@users.noreply.github.com>
2022-05-11 13:30:13 -0400

    adding last test

--- js/pages/support.js
+++ js/pages/support.js
@@ -124,26 +124,6 @@ export const renderSupportPage = async () => {
                 <p>
                     You can also call ${phone} or email <a href = "mailto:ConnectSupport@norc.org">ConnectSupport@norc.org</a>
                 </p>
-                <input type="time" style="size:20 !important"></input>
-                <br>
-                <input type="time"></input>
-                <br>
-                <input type="time" style="size:20px !important"></input>
-                <br>
-                <input type="time" style="size:100 !important"></input>
-                <br>
-                <input type="time" style="size:100px !important"></input>
-                <br>
-                <input type="time" style="height:30px !important; min-width: 150px;"></input>
-                <br>
-                <input type="time" style="height:30px !important"></input>
-                <br>
-                <input type="time" style="height:30px"></input>
-                <br>
-                <input type="time" style="min-height:30px"></input>
-
-
-
 
             </div>
             <!--
