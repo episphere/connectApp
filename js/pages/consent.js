ba65099dabd4426e5da6757b99f707a3f1e4f803
Brian S <27040828+naiyume@users.noreply.github.com>
2022-05-31 13:31:56 -0400

    adding fix to make pdfs higher res

--- js/pages/consent.js
+++ js/pages/consent.js
@@ -1115,11 +1115,14 @@ function renderPage(num, pageIndicator, pdfDoc, viewer) {
       viewer.appendChild(canvas);
       //console.log('consoleWidth: ' + viewer.clientWidth + ', ' +viewer.clientWidth / page.getViewport(1.0).width)
       var viewport = page.getViewport(Math.max(viewer.clientWidth / page.getViewport(1.0).width, 0.7));
-      var rect = canvas.getBoundingClientRect();
       //canvas.width = Math.round (devicePixelRatio * rect.right)
         //        - Math.round (devicePixelRatio * rect.left);
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
+      //canvas.style = `height:${viewport.height}px; width:${viewport.width}px;`
+      canvas.style.height = viewport.height + 'px';
+      canvas.style.width = viewport.width + 'px';
+      canvas.height = 2*viewport.height;
+      canvas.width = 2*viewport.width;
+      
       //canvas.height = Math.round (devicePixelRatio * rect.bottom)
       //          - Math.round (devicePixelRatio * rect.top);
       viewer.style = `min-height:${Math.min(viewport.height, 500)}px;max-height:${Math.min(viewport.height + 50, 500)}px;`
@@ -1130,7 +1133,7 @@ function renderPage(num, pageIndicator, pdfDoc, viewer) {
       var renderContext = {
         canvasContext: canvas.getContext('2d'),
         viewport: viewport,
-        //transform: [2,0,0,2,0,0]
+        transform: [2,0,0,2,0,0]
       };
       var renderTask = page.render(renderContext);
 
