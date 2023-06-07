db14a146a9d07c8b8ae97e62c810d332d18aaaed
Vaishnavi <vaishuramavarapu@gmail.com>
2023-06-06 22:58:25 -0400

    Made the required changes in the durations and location.

--- js/pages/samples.js
+++ js/pages/samples.js
@@ -328,7 +328,7 @@ const marshfield = {
     name: 'Marshfield',
     donatingSamples: 'As part of Connect, we ask you to donate blood, urine, and saliva samples and complete a short survey.',
     whenToDonate: 'The Connect team will send you an email when it is time to donate your samples. Be sure to check your spam or junk folder. After you receive the email, it is important to donate your samples as soon as you can. It is easy to donate all of your samples in one visit.<br><br><span style="font-weight:900; text-decoration:underline">Note:</span> If you have recently had a blood transfusion or donated blood, please wait at least <span style="font-weight:900; text-decoration:underline">eight weeks</span> from your donation or transfusion before donating your samples for Connect. If you have recently donated plasma, please wait at least <span style="font-weight:900; text-decoration:underline">two days</span> from your plasma donation before donating samples for Connect. If you have an upcoming colonoscopy, please be sure that you <span style="font-weight:900; text-decoration:underline">do not</span> donate samples for Connect on the <span style="font-weight:900; text-decoration:underline">same day</span> as your colonoscopy.',
-    howToDonate: 'The email we send you will contain a link to schedule an appointment. Simply click the link to schedule a time that is convenient for you to donate your samples.You can also  please call Marshfield Clinic Research Institute at 715-898-9444 to schedule an appointment, or a Connect team member will call you to schedule an appointment to donate your samples at a time that is convenient for you.',
+    howToDonate: 'The email we send you will contain a link to schedule an appointment. Simply click the link to schedule a time that is convenient for you to donate your samples.You can also call Marshfield Clinic Research Institute at 715-898-9444 to schedule an appointment, or a Connect team member will call you to schedule an appointment to donate your samples at a time that is convenient for you.',
     scheduling: 'For questions and scheduling please call: 715-898-9444 or email <a href="mailto: connectstudy@marshfieldresearch.org">connectstudy@marshfieldresearch.org</a>.<br><br>All patients and visitors are required to wear a mask. If you enter the building without a mask, we will give one to you. If you are unable to wear a mask for the duration of the visit, we ask that you do not schedule your visit at this time.',
     howLong: 'Please expect to spend an average of one hour at your appointment to donate your samples and complete a short survey.',
     prepareInstructions: 'On the day of your appointment, please drink plenty of water, but <span style="font-weight:900; text-decoration:underline">stop drinking water one hour before your appointment.</span><br><br><span style="font-weight:900; text-decoration:underline">One hour before your appointment:</span> Please <span style="font-weight:900; text-decoration:underline">do not</span> eat, drink any liquids (including water), chew gum, smoke, vape, or chew any products (including tobacco), rinse your mouth, or brush your teeth.<br><br><span style="font-weight:900; text-decoration:underline">Things to bring and remember:</span><br><br><ul><li>Make sure you know your login information for the MyConnect app.</li><li>We will ask you to complete a short survey when you donate your samples. It may be helpful to have this information on hand:<ul><li>The last time you ate or drank, and the times you went to sleep the night before your appointment and woke up on the day of your appointment.</li><li>If you are menstruating, the start date of your most recent menstrual period in the last 12 months.</li><li>Information and dates regarding COVID-19 testing, symptoms, recovery information (including any hospital stay and treatment), and your vaccination status.</li></ul></li></ul>',
@@ -344,19 +344,19 @@ const marshfield = {
         [
             'Lake Hallie Center',
             '12961 27th Ave<br>Chippewa Falls, WI 54729',
-            'Monday, Tuesday, Thursday: 8:00 a.m. - 2:00 p.m. *please ask about early morning or evening appointment availability if needed',
+            'Monday, Tuesday, Thursday: 8:00 a.m. - 2:00 p.m.<br> Wednesday: 7:00 a.m. - 2:00 p.m.',
             'General parking available'
         ],
         [
             'Weston Center',
             '3400 Ministry Pkwy<br>Weston, WI 54476',
-            'Monday - Friday: 8:00 a.m. to 2:00 p.m. *please ask about early morning or evening appointment availability if needed',
+            'Mondays and Thursdays: 8:00 a.m. to 1:00 p.m. *please ask about evening and Friday appointment availability if needed',
             'General parking available'
         ],
         [
             'Wisconsin Rapids Center',
             '220 24th St S,<br>Wisconsin Rapids,WI 54494',
-            'Thursdays: 7:00 a.m. to 41:00 p.m. <br> Fridays: 7:00 a.m. to 10:00 a.m.',
+            'Thursdays: 7:00 a.m. to 4:00 p.m. <br> Fridays: 7:00 a.m. to 10:00 a.m.',
             'General parking available'
 
 
