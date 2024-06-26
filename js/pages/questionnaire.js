a2d7b66791961b1ffe4776d622387d74ec5eb1c4
anthonypetersen <tonypetersen@hotmail.com>
2024-06-26 12:42:26 -0500

    updating dev quest fallback

--- js/pages/questionnaire.js
+++ js/pages/questionnaire.js
@@ -32,7 +32,7 @@ async function loadQuestConfig() {
         questConfig = {
             "myconnect.cancer.gov": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.1.9/replace2.js",
             "myconnect-stage.cancer.gov": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.1.9/replace2.js",
-            "episphere.github.io": "https://episphere.github.io/quest/replace2.js",
+            "episphere.github.io": "https://episphere.github.io/quest-dev/replace2.js",
             "localhost:5000": "https://cdn.jsdelivr.net/gh/episphere/quest@v1.1.9/replace2.js"
         }
     }
